6f402bf366f541d6c9145b724a7c7988be329edc
Alikhan Mukhanaliyev <granit.nc@gmail.com>
2020-08-14 09:52:50 +0600

    Specified return type explicitly because of platform call

--- app/src/main/java/com/ietze/reminder/utils/FragmentExtensions.kt
+++ app/src/main/java/com/ietze/reminder/utils/FragmentExtensions.kt
@@ -1,5 +1,6 @@
 package com.ietze.reminder.utils
 
+import android.app.Application
 import androidx.fragment.app.Fragment
 
-fun Fragment.requireApplication() = requireActivity().application
+fun Fragment.requireApplication(): Application = requireActivity().application
